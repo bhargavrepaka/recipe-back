9b997b126c5eff4d49b64e524617cc92d52a1e2b
Repaka Bhargav <repaka.bhargav@gmail.com>
2024-03-03 18:56:03 +0530

    commit

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
-import User from '../models/User.model.js';
+import User from '../models/user.model.js';
 import bcrypt from 'bcrypt'
 const signIn = async (req, res) => {
     const { email, password } = req.body;
