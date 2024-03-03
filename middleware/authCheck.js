3fcbeddb6e3aee8f27d5a1a385b5c5a5e91236bc
Repaka Bhargav <repaka.bhargav@gmail.com>
2024-03-03 18:57:31 +0530

    commit

--- middleware/authCheck.js
+++ middleware/authCheck.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
-import User from '../models/User.model.js';
+import User from '../models/user.model.js';
 
 const authCheck = async (req, res, next) => {
     const authToken = req.headers.authorization;
